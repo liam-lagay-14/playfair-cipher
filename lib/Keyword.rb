019eaaca9c2d78ab602b8d22104ca40e882b0ef2
Liam Lagay <liam.lagay@sage.com>
2014-09-09 08:25:07 +0100

    Adding in keyword specs + code. Also still trying to fix this bloody .idea gitignore issue

--- lib/Keyword.rb
+++ lib/Keyword.rb
@@ -2,5 +2,11 @@ class Keyword
   attr_accessor :keyword
   def initialize(keyword)
     @keyword = keyword
+
+    raise ArgumentError, "Keyword must contain a value" unless keyword.length > 0
+  end
+
+  def is_valid_keyword?
+    (keyword =~ /^[A-Z]+$/) == 0
   end
 end
--- specs/keyword_spec.rb
+++ specs/keyword_spec.rb
@@ -4,7 +4,25 @@ require_relative '../lib/Keyword'
 describe Keyword do
   let(:valid_keyword) { Keyword.new('CHARLES') }
 
+  let(:invalid_keyword) { Keyword.new('FAILURETEST123') }
+
+  let(:blank_keyword) { Keyword.new('') }
+
   it 'takes a string and stores a value' do
      expect(valid_keyword.keyword).to eq('CHARLES')
+  end
+
+  it 'only allows an uppercase value' do
+    expect(valid_keyword.is_valid_keyword?).to eq(true)
+  end
+
+  it 'returns false for keywords containing anything except letters' do
+    expect(invalid_keyword.is_valid_keyword?).to eq(false)
+  end
+
+  context 'raise an argument error if a blank keyword is entered' do
+    subject { blank_keyword }
+
+    it { expect { subject }.to raise_error(ArgumentError) }
   end
 end
