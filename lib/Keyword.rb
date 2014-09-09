2ddba7aaa273796684fa0fa7405dd83e82319971
Liam Lagay <liam.lagay@sage.com>
2014-09-09 08:31:46 +0100

    Small updates to keyword file and gitignore file

--- lib/Keyword.rb
+++ lib/Keyword.rb
@@ -3,7 +3,7 @@ class Keyword
   def initialize(keyword)
     @keyword = keyword
 
-    raise ArgumentError, "Keyword must contain a value" unless keyword.length > 0
+    raise ArgumentError, 'Keyword must contain a value' unless keyword.length > 0
   end
 
   def is_valid_keyword?
